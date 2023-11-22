4215b374b29e0866a28b9778e495eae38a02e561
Shivang <shivangraghuvanshi2005@gmail.com>
2023-11-22 23:01:40 +0530

    Added shading with surface normals

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,26 @@ use ray::Ray;
 use vec3::{Vec3, Point3};
 
 // To tell if the ray hits the sphere or not (retuns a boolean value)
-fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - center;
-    let a = vec3::dot(r.direction(), r.direction());
-    let b = 2.0 * vec3::dot(oc, r.direction());
-    let c = vec3::dot(oc, oc) - (radius * radius);
-    let discriminant  = (b * b) - (4.0 * a * c);
-    discriminant >= 0.0
-}
+    let a = r.direction().length_squared();
+    let half_b = vec3::dot(oc, r.direction());
+    let c = oc.length_squared() - (radius * radius);
+    let discriminant  = (half_b * half_b) - (a * c);
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - f64::sqrt(discriminant)) / a
+    }
+}   
 
 // To determine the color of the ray
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
-    } 
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
+    }
 
     let unit_direction = vec3::unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
